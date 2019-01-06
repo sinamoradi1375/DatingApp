9307ca27f9c986981837ec68956fe79208948c71
sina <sina.moradii1375@gmail.com>
2019-01-06 20:31:30 +0330

    End of Section 7

---
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private alertify: AlertifyService,
+    private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.authService.loggedIn()) {
+      return true;
+    }
+
+    this.alertify.error('خطا! برای استفاده از اپلیکیشن ابتدا باید وارد شوید.');
+    this.router.navigate(['']);
+  }
+}
