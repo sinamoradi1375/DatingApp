acd7e9a9a1a8480ae8a1a205a7b36f0136563140
sinamoradi1375 <sina.moradii1375@gmail.com>
2019-07-13 16:33:03 +0430

    98/4/22

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -57,6 +58,7 @@ export function tokenGetter() {
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -2,22 +2,51 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { Router } from '@angular/router';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
+  animations: [
+  trigger('collapse', [
+    state('open', style({
+      opacity: '1',
+      display: 'block',
+      transform: 'translate3d(0, 0, 0)'
+    })),
+    state('closed',   style({
+      opacity: '0',
+      display: 'none',
+      transform: 'translate3d(0, -100%, 0)'
+    })),
+    transition('closed => open', animate('100ms ease-in')),
+    transition('open => closed', animate('300ms ease-out'))
+  ])
+]
 })
+
 export class NavComponent implements OnInit {
   model: any = {};
   photoUrl: string;
+  collapse = 'open';
 
-  constructor(public authService: AuthService, private alertify: AlertifyService, 
+  constructor(public authService: AuthService, private alertify: AlertifyService,
     private router: Router) { }
 
   ngOnInit() {
     this.authService.currentPhotoUrl.subscribe(res => this.photoUrl = res);
   }
+
+  toggleCollapse() {
+    this.collapse = this.collapse === 'open' ? 'closed' : 'open';
+    }
 
   login() {
     this.authService.login(this.model).subscribe(next => {
